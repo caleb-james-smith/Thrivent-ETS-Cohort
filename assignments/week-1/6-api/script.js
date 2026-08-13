043405043c29fe36f924697a2639efb633999e60
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-08-13 17:34:12 -0500

    API demo: get user data and log user info.

--- assignments/week-1/6-api/script.js
+++ assignments/week-1/6-api/script.js
@@ -1,18 +1,35 @@
 const loadUsersButton = document.getElementById("load-users");
 
 async function loadData() {
+    console.log("Loading user data...");
+
     // const url = "FAKE_URL"; // Test fake URL
     const url = "https://jsonplaceholder.typicode.com/users";
+    
     try {
         const response = await fetch(url);
+
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
+
+        const user_data = await response.json();
+        const num_users = user_data.length;
+
+        console.log(`Number of users: ${num_users}`);
+        
+        for (let i = 0; i < num_users; i++) {
+            const name = user_data[i]["name"];
+            const email = user_data[i]["email"];
+            console.log(`${i}: ${name}, ${email}`);
+        }
+
     } catch (error) {
         console.log("Start of catch().");
         console.error(error.message);
         console.log("End of catch().");
     }
+    console.log("Done!");
 }
 
 loadUsersButton.addEventListener("click", loadData);
