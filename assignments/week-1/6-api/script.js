9740e6ab1aef8df42aaf17887bd202e1c6472656
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-08-14 12:53:46 -0500

    API demo: create function to update results on the webpage.

--- assignments/week-1/6-api/script.js
+++ assignments/week-1/6-api/script.js
@@ -1,35 +1,56 @@
 const loadUsersButton = document.getElementById("load-users");
+const results = document.getElementById("results");
 
 async function loadData() {
     console.log("Loading user data...");
 
+    // URL for HTTP request
     // const url = "FAKE_URL"; // Test fake URL
     const url = "https://jsonplaceholder.typicode.com/users";
+    const user_data = await getData(url);
+
+    updateResults(user_data);
     
-    try {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-
-        const user_data = await response.json();
-        const num_users = user_data.length;
-
-        console.log(`Number of users: ${num_users}`);
-        
-        for (let i = 0; i < num_users; i++) {
-            const name = user_data[i]["name"];
-            const email = user_data[i]["email"];
-            console.log(`${i}: ${name}, ${email}`);
-        }
-
-    } catch (error) {
-        console.log("Start of catch().");
-        console.error(error.message);
-        console.log("End of catch().");
-    }
     console.log("Done!");
+}
+
+async function getData(url) {
+    let data = [];
+    try {
+        // Send HTTP request to URL using fetch()
+        const response = await fetch(url);
+        console.log(`response status: ${response.status}`);
+        if (!response.ok) {
+            throw new Error(`Response was not OK; response status: ${response.status}`);
+        }
+
+        // Get data from response in JSON format
+        data = await response.json();
+    } catch (error) {
+        console.error(error.message);
+    }
+    return data;
+}
+
+function updateResults(user_data) {
+    const num_users = user_data.length;
+    const text_num_users = `Number of users: ${num_users}`;
+    addToResults(text_num_users, "p");
+    console.log(text_num_users);
+
+    for (let i = 0; i < num_users; i++) {
+        const name = user_data[i]["name"];
+        const email = user_data[i]["email"];
+        const text_results = `${i}: ${name}, ${email}`;
+        addToResults(text_results, "p");
+        console.log(text_results);
+    }
+}
+
+function addToResults(message, tag) {
+    const new_element = document.createElement(tag);
+    new_element.textContent = message;
+    results.appendChild(new_element);
 }
 
 loadUsersButton.addEventListener("click", loadData);
