91e443ae6a7e735de2d215031a34d58d475e6536
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-08-17 13:47:33 -0500

    Advanced JS: Create functions to display user profile, login status, welcome, nickname, address, and hobbies.

--- assignments/week-2/1-advanced-js/script.js
+++ assignments/week-2/1-advanced-js/script.js
@@ -1,7 +1,75 @@
 const changeThemeButton = document.getElementById("change-theme");
+const loginStatusElement = document.getElementById("login-status");
+const welcomeElement = document.getElementById("welcome");
+const nicknameElement = document.getElementById("nickname");
+const addressElement = document.getElementById("address");
+const hobbiesElement = document.getElementById("hobbies");
+
+const user = {
+    name: "Caleb Smith",
+    age: 33,
+    isLoggedIn: true,
+    address: {
+        city: "Lawrence",
+        state: "Kansas"
+    },
+    preferences: {
+        theme: "dark-mode",
+        nickname: "Jimmy"
+        // nickname: null
+    },
+    hobbies: ["ultimate frisbee", "swing dancing", "speedcubing", "chess", "StarCraft II"]
+    // hobbies: []
+};
+
+function displayUserProfile(user) {
+    displayLoginStatus(user.isLoggedIn);
+    if (user.isLoggedIn) {
+        displayWelcome(user.name);
+        displayNickname(user.preferences.nickname);
+        displayAddress(user.address.city, user.address.state);
+        displayHobbies(user.hobbies);
+    }
+}
+
+function displayLoginStatus(isLoggedIn) {
+    if (isLoggedIn) {
+        loginStatusElement.innerHTML = `<b>You are logged in.</b>`;
+        loginStatusElement.style.color = "green";
+    } else {
+        loginStatusElement.innerHTML = `<b>You are not logged in.</b>`;
+        loginStatusElement.style.color = "red";
+    }
+}
+
+function displayWelcome(name) {
+    welcomeElement.textContent = `Hello, ${name}!`;
+}
+
+function displayNickname(nickname) {
+    if (nickname) {
+        nicknameElement.textContent = `Nickname: ${nickname}`;
+    } else {
+        nicknameElement.textContent = `Nickname: Guest`;
+    }
+}
+
+function displayAddress(city, state) {
+    addressElement.textContent = `Address: ${city}, ${state}`;
+}
+
+function displayHobbies(hobbies) {
+    if (hobbies) {
+        hobbiesElement.textContent = `Hobbies: ${hobbies.join(", ")}`;
+    } else {
+        hobbiesElement.textContent = `Hobbies: None`;
+    }
+}
 
 function changeTheme() {
    document.body.classList.toggle("dark-mode");
 }
+
+displayUserProfile(user);
 
 changeThemeButton.addEventListener("click", changeTheme);
