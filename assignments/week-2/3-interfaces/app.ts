b3653830f5616cad3cd112f130a7a10fba479274
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-08-21 11:09:13 -0500

    Start week 2, assignment 3: Interfaces.

---
+++ assignments/week-2/3-interfaces/app.ts
@@ -0,0 +1,13 @@
+// Learning Platform Data Model App
+
+function runApp(): void {
+    printHeader();
+}
+
+function printHeader(): void {
+    console.log("------------------------------------");
+    console.log("| Learning Platform Data Model App |");
+    console.log("------------------------------------");
+}
+
+runApp();
