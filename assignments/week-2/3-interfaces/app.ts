f1c7fece99c130a784b303e857e4f3c3ce4c2d42
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-08-21 15:19:12 -0500

    Interfaces: Add properties to student, instructor, and course. Create derived types for summaries.

--- assignments/week-2/3-interfaces/app.ts
+++ assignments/week-2/3-interfaces/app.ts
@@ -8,13 +8,18 @@ interface Person {
 }
 
 interface Student extends Person {
+    gpa: number,
+    creditsEarned: number
 }
 
 interface Instructor extends Person {
+    jobTitle: string;
 }
 
 interface Course {
     subject: string;
+    credits: number;
+    location: string;
     instructor: Instructor;
     students: Student[];
 }
@@ -24,41 +29,54 @@ interface Lesson {
     topic: string
 }
 
+type CourseSummary = Omit<Course, "credits" | "location">;
+type StudentSummary = Omit<Student, "email">;
+
 // Students
 
 const AndrewWiggin: Student = {
     id: 5001,
     firstName: "Andrew",
     lastName: "Wiggin",
-    email: "andrew.wiggin@ku.edu"
+    email: "andrew.wiggin@ku.edu",
+    gpa: 4.00,
+    creditsEarned: 80
 }
 
 const EmilyJohnson: Student = {
     id: 5002,
     firstName: "Emily",
     lastName: "Johnson",
-    email: "emily.johnson@ku.edu"
+    email: "emily.johnson@ku.edu",
+    gpa: 3.25,
+    creditsEarned: 40
 }
 
 const LilyPotter: Student = {
     id: 5003,
     firstName: "Lily",
     lastName: "Potter",
-    email: "lily.potter@ku.edu"
+    email: "lily.potter@ku.edu",
+    gpa: 4.00,
+    creditsEarned: 60
 }
 
 const SamwiseGamgee: Student = {
     id: 5004,
     firstName: "Samwise",
     lastName: "Gamgee",
-    email: "samwise.gamgee@ku.edu"
+    email: "samwise.gamgee@ku.edu",
+    gpa: 3.00,
+    creditsEarned: 50
 }
 
 const ThomasScott: Student = {
     id: 5005,
     firstName: "Thomas",
     lastName: "Scott",
-    email: "thomas.scott@ku.edu"
+    email: "thomas.scott@ku.edu",
+    gpa: 3.75,
+    creditsEarned: 100
 }
 
 // Instructors
@@ -67,24 +85,30 @@ const AmyJones: Instructor = {
     id: 1001,
     firstName: "Amy",
     lastName: "Jones",
-    email: "amy.jones@ku.edu"
+    email: "amy.jones@ku.edu",
+    jobTitle: "Associate Professor"
 }
 
 const CalebSmith: Instructor = {
     id: 1002,
     firstName: "Caleb",
     lastName: "Smith",
-    email: "caleb.smith@ku.edu"
+    email: "caleb.smith@ku.edu",
+    jobTitle: "Assistant Professor"
 }
 
 const Calculus1: Course = {
     subject: "Calculus 1",
+    credits: 3,
+    location: "Mathematics Building 1",
     instructor: AmyJones,
     students: [EmilyJohnson, SamwiseGamgee, ThomasScott]
 }
 
 const ModernPhysics: Course = {
     subject: "Modern Physics",
+    credits: 4,
+    location: "Physics Building 1",
     instructor: CalebSmith,
     students: [AndrewWiggin, LilyPotter, ThomasScott]
 }
@@ -93,16 +117,20 @@ function runApp(): void {
     const students: Student[] = [AndrewWiggin, EmilyJohnson, LilyPotter, SamwiseGamgee, ThomasScott];
     const instructors: Instructor[] = [AmyJones, CalebSmith];
     const courses: Course[] = [Calculus1, ModernPhysics];
+
     printHeader();
     for (const course of courses) {
         printCourseSummary(course);
     }
+
+    console.log("------------------------------------");
+
     for (const student of students) {
         printStudentSummary(student);
     }
 }
 
-function printCourseSummary(course: Course): void {
+function printCourseSummary(course: CourseSummary): void {
     console.log();
     console.log(`Subject: ${course.subject}`);
     console.log(`Instructor: ${course.instructor.firstName} ${course.instructor.lastName} (${course.instructor.email})`);
@@ -110,9 +138,11 @@ function printCourseSummary(course: Course): void {
     console.log();
 }
 
-function printStudentSummary(student: Student): void {
+function printStudentSummary(student: StudentSummary): void {
     console.log();
-    console.log(`Student: ${student.firstName} ${student.lastName} (${student.email})`);
+    console.log(`Student: ${student.firstName} ${student.lastName}`);
+    console.log(`GPA: ${student.gpa}`);
+    console.log(`Credits earned: ${student.creditsEarned}`);
     console.log();
 }
 
