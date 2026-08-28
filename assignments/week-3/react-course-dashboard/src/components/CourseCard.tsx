8747c35b91033794da629291677d9c31d1408e36
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-08-28 14:39:13 -0500

    Use flexbox (column) for course card. Adjust spacing, and use gap instead of padding.

--- assignments/week-3/react-course-dashboard/src/components/CourseCard.tsx
+++ assignments/week-3/react-course-dashboard/src/components/CourseCard.tsx
@@ -9,16 +9,14 @@ export default function CourseCard({
   updateEnrolledCourses,
 }: CourseProps) {
   return (
-    <div className="course-card shadow-xl hover:shadow-2xl">
-      <h2 className="font-bold text-2xl text-gray-800 text-center p-2">
+    <div className="course-card shadow-xl hover:shadow-2xl flex flex-col items-start gap-2">
+      <h2 className="font-bold text-2xl text-gray-800 text-center p-2 self-center">
         {course.title}
       </h2>
       {course.featured && (
-        <div className="p-2">
-          <p className="text-green-600">
-            <b>Featured Course</b>
-          </p>
-        </div>
+        <p className="text-green-600">
+          <b>Featured Course</b>
+        </p>
       )}
       <button type="button" onClick={() => updateSelectedCourse(course)}>
         View Details
--- assignments/week-3/react-course-dashboard/src/components/CourseDetails.tsx
+++ assignments/week-3/react-course-dashboard/src/components/CourseDetails.tsx
@@ -13,7 +13,7 @@ export default function CourseDetails({
     }
   }
   return (
-    <div className="flex flex-col items-start p-2 gap-3">
+    <div className="flex flex-col items-start gap-2">
       <p>{course.description}</p>
       <p>Duration: {course.duration} hours</p>
       <p>Level: {course.level}</p>
