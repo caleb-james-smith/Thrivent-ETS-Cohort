00ac5682e99cda378fc3dea5590c46da70c77c75
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-09-16 21:16:20 -0500

    Create function to set up store. Create a new store for each test. Finish article card test: define expected text and add expect() assertions.

--- assignments/week-4/redux-state-practice/src/features/articles/ArticleCard.test.tsx
+++ assignments/week-4/redux-state-practice/src/features/articles/ArticleCard.test.tsx
@@ -2,16 +2,27 @@ import { render, screen } from "@testing-library/react";
 import type { Article } from "../../types/types";
 import { ArticleCard } from "./ArticleCard";
 import { Provider } from "react-redux";
-import { store } from "../../app/store";
+import { setupStore } from "../../app/store";
 
 describe("Article Card", () => {
-  it("renders title, category, and saved", async () => {
+  it("renders article info", () => {
     // Arrange
+
+    // Create a new store for each test
+    const store = setupStore();
+
+    const testTitle = "Test Article";
+    const testCategory = "testing";
+    const testSaved = false;
+    const expectedTextCategory = `Category: ${testCategory}`;
+    // const expectedTextSaved = (testSaved ? "Saved: Yes" : "Saved: No");
+    const expectedTextSaved = "Saved: " + (testSaved ? "Yes" : "No");
+
     const article: Article = {
       id: "1",
-      title: "Test Article",
-      category: "testing",
-      saved: false
+      title: testTitle,
+      category: testCategory,
+      saved: testSaved
     };
 
     // Act
@@ -22,6 +33,8 @@ describe("Article Card", () => {
   );
 
     // Assert
-
+    expect(screen.getByText(testTitle)).toBeInTheDocument();
+    expect(screen.getByText(expectedTextCategory)).toBeInTheDocument();
+    expect(screen.getByText(expectedTextSaved)).toBeInTheDocument();
   });
 });
