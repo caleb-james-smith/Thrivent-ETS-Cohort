272343dd39ab01c2e62b7577287f3173fc7e8761
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-09-23 15:29:18 -0500

    Add SQL script for tech talk (before aggregating data).

---
+++ assignments/week-6/fitness_booking/scripts_read_data/Tech-Talk-1.sql
@@ -0,0 +1,20 @@
+-- Tech Talk 1
+
+-- Get info for class sessions
+-- Include the number of bookings per session
+-- Sort by number of bookings in descending order
+-- Include class session with no bookings
+
+SELECT
+	--COUNT(bookings.class_session_id) AS num_bookings,
+	classes.name AS class_name,
+	class_types.type AS class_type,
+	locations.name AS location_name,
+	class_sessions.start_at,
+	class_sessions.end_at
+FROM class_sessions
+JOIN classes ON class_sessions.class_id = classes.id
+JOIN locations ON class_sessions.location_id = locations.id
+JOIN class_types ON class_types.id = classes.class_type_id;
+--JOIN bookings ON bookings.class_session_id = class_sessions.id;
+
