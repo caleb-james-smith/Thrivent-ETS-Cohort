c259f7d89d1efa970ef4bc68b87d3036b7e3fd7a
Caleb James Smith <colorado.caleb.smith@gmail.com>
2026-09-24 09:50:20 -0500

    Update SQL script for tech talk with solution to aggregate data.

--- assignments/week-6/fitness_booking/scripts_read_data/Tech-Talk-1.sql
+++ assignments/week-6/fitness_booking/scripts_read_data/Tech-Talk-1.sql
@@ -6,7 +6,7 @@
 -- Include class session with no bookings
 
 SELECT
-	--COUNT(bookings.class_session_id) AS num_bookings,
+	COUNT(bookings.class_session_id) AS num_bookings,
 	classes.name AS class_name,
 	class_types.type AS class_type,
 	locations.name AS location_name,
@@ -15,6 +15,12 @@ SELECT
 FROM class_sessions
 JOIN classes ON class_sessions.class_id = classes.id
 JOIN locations ON class_sessions.location_id = locations.id
-JOIN class_types ON class_types.id = classes.class_type_id;
---JOIN bookings ON bookings.class_session_id = class_sessions.id;
-
+JOIN class_types ON class_types.id = classes.class_type_id
+LEFT JOIN bookings ON bookings.class_session_id = class_sessions.id
+GROUP BY
+	class_name,
+	class_type,
+	location_name,
+	class_sessions.start_at,
+	class_sessions.end_at
+ORDER BY num_bookings DESC;
